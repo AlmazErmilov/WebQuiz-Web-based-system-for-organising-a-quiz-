5fa83f2a9dabed730c542e943a1a4d64e323ba95
Jøran Olaisen <jol105@uit.no>
2023-04-14 16:17:55 +0200

    added utils.py file cleaned comments

--- app.py
+++ app.py
@@ -1,21 +1,11 @@
 from flask import Flask, render_template, request, redirect, url_for, flash
-import mysql.connector
-from db_connection_config import HOST, USER, PASSWORD, DATABASE 
 from datetime import datetime
+from utils import *
 
 app = Flask(__name__)
 app.secret_key = 'quiz'
 app.config['APPLICATION_ROOT'] = '/templates'
 
-def get_db_connection():
-    connection = mysql.connector.connect(
-        host=HOST,
-        user=USER,
-        password=PASSWORD,
-        database=DATABASE)
-    return connection
-
-# Main page route
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
@@ -34,37 +24,18 @@ def index():
 def admin():
     admin_name = request.args.get('admin_name', default=None)
 
-    connection = get_db_connection()
-    cursor = connection.cursor()
-    cursor.execute("SELECT * FROM questions")
-
-    questions_raw = cursor.fetchall()
+    questions_raw = fetch_query_results("SELECT * FROM questions")
     questions = [dict(id=row[0], quiz_id=row[1], question_text=row[2], answer=row[3], category=row[4]) for row in questions_raw]
 
-    cursor.close()                      
-    connection.close()
-
-    #code for showing answers.
-    connection = get_db_connection()
-    cursor = connection.cursor()
-    cursor.execute("SELECT * FROM quizzes")
-    quizzes = cursor.fetchall()
-    cursor.close()
-    connection.close()
+    quizzes = fetch_query_results("SELECT * FROM quizzes")
 
     return render_template('admin.html', questions=questions, quizzes=quizzes, admin_name=admin_name)
 
 @app.route('/user')
 def user():
-    connection = get_db_connection()
-    cursor = connection.cursor()
-    cursor.execute("SELECT * FROM quizzes")
-    quizzes = cursor.fetchall()
-    cursor.close()
-    connection.close()
+    quizzes = fetch_query_results("SELECT * FROM quizzes")
     return render_template('user.html', quizzes=quizzes)
 
-# Function to create a new question
 @app.route('/create_question', methods=['POST'])
 def create_question():
     category = request.form['category']
@@ -94,7 +65,6 @@ def create_question():
     flash('Question created successfully')
     return redirect(url_for('admin'))
 
-# Function to edit a question
 @app.route('/edit_question/<int:question_id>', methods=['GET', 'POST'])
 def edit_question(question_id):
     connection = get_db_connection()
@@ -115,34 +85,21 @@ def edit_question(question_id):
         question_dict = dict(id=question[0], quiz_id=question[1], question_text=question[2], answer=question[3], category=question[4]) 
         return render_template('edit_question.html', question=question_dict)
 
-# Function to delete a question
 @app.route('/delete_question/<int:question_id>', methods=['GET', 'POST'])
 def delete_question(question_id):
-    connection = get_db_connection()
-    cursor = connection.cursor()
-    cursor.execute("DELETE FROM questions WHERE id=%s", (question_id,))
-    connection.commit()
-    cursor.close()
-    connection.close()
+    execute_query("DELETE FROM user_answers WHERE question_id=%s", (question_id,))
+    execute_query("DELETE FROM questions WHERE id=%s", (question_id,))
     flash('Question deleted successfully')
     return redirect(url_for('admin'))
 
-# Function to start_quiz
 @app.route('/start_quiz', methods=['POST'])
 def start_quiz():
     quiz_id = request.form['quiz-select']
     
-    connection = get_db_connection()
-    cursor = connection.cursor()
-    cursor.execute("SELECT category FROM quizzes WHERE id=%s", (quiz_id,))
-    # quiz_category = cursor.fetchone()[0]
-    quiz_category = cursor.fetchall()[0][0]
-    cursor.close()
-    connection.close()
+    quiz_category = fetch_query_results("SELECT category FROM quizzes WHERE id=%s", (quiz_id,))[0][0]
     
     return redirect(url_for('display_quiz', quiz_id=quiz_id))
 
-# Function to display a quiz
 @app.route('/quiz/<int:quiz_id>', methods=['GET'])
 def display_quiz(quiz_id):
     connection = get_db_connection()
@@ -161,19 +118,13 @@ def admin_quiz_details():
     else:
         return redirect(url_for('admin'))
 
-    connection = get_db_connection()
-    cursor = connection.cursor()
-    cursor.execute("""SELECT ua.id, q.question_text, ua.answer, q.answer, ua.created_at  
-                      FROM user_answers ua 
-                      JOIN questions q ON ua.question_id = q.id 
-                      WHERE ua.quiz_id = %s""", (quiz_id,))
-    user_answers = cursor.fetchall()
-    cursor.close()
-    connection.close()
+    user_answers = fetch_query_results("""SELECT ua.id, q.question_text, ua.answer, q.answer, ua.created_at  
+                                          FROM user_answers ua 
+                                          JOIN questions q ON ua.question_id = q.id 
+                                          WHERE ua.quiz_id = %s""", (quiz_id,))
 
     return render_template('admin_quiz_details.html', user_answers=user_answers)
 
-# Function to submit a quiz
 @app.route('/submit_quiz', methods=['POST'])
 def submit_quiz():
     quiz_id = request.form['quiz_id']
@@ -196,6 +147,5 @@ def submit_quiz():
     flash('Quiz submitted successfully')
     return redirect(url_for('user'))
 
-# Run the Flask application
 if __name__ == '__main__':
     app.run(debug=True)
---
+++ utils.py
@@ -0,0 +1,34 @@
+import mysql.connector
+from db_connection_config import HOST, USER, PASSWORD, DATABASE
+
+def get_db_connection():
+    connection = mysql.connector.connect(
+        host=HOST,
+        user=USER,
+        password=PASSWORD,
+        database=DATABASE)
+    return connection
+
+def fetch_query_results(query, params=None):
+    connection = get_db_connection()
+    cursor = connection.cursor()
+    if params:
+        cursor.execute(query, params)
+    else:
+        cursor.execute(query)
+    results = cursor.fetchall()
+    cursor.close()
+    connection.close()
+    return results
+
+def execute_query(query, params=None):
+    connection = get_db_connection()
+    cursor = connection.cursor()
+    if params:
+        cursor.execute(query, params)
+    else:
+        cursor.execute(query)
+    connection.commit()
+    cursor.close()
+    connection.close()
+
